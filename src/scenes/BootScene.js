9a32b4c8d5a981aaf8f60ca5ae0811efd9e0d281
Archisha <archisha890@gmail.com>
2026-06-09 22:26:52 +0530

    Fixed start point

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -51,10 +51,10 @@ export class BootScene extends Phaser.Scene {
       Promise.race([
         document.fonts.ready,
         new Promise((resolve) => window.setTimeout(resolve, 1200))
-      ]).then(() => this.scene.start("GalleryScene"));
+      ]).then(() => this.scene.start("MainMenuScene"));
       return;
     }
 
-    this.scene.start("GalleryScene");
+    this.scene.start("MainMenuScene");
   }
 }
