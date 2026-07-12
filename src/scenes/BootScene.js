4129694a897a158c1fb15388a0d350487baa3a22
Archisha <archisha890@gmail.com>
2026-07-12 18:46:43 +0530

    commit

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -77,10 +77,10 @@ export class BootScene extends Phaser.Scene {
       Promise.race([
         document.fonts.ready,
         new Promise((resolve) => window.setTimeout(resolve, 1200))
-      ]).then(() => this.scene.start("MainMenuScene"));
+      ]).then(() => this.scene.start("BedroomScene"));
       return;
     }
 
-    this.scene.start("MainMenuScene");
+    this.scene.start("BedroomScene");
   }
 }
